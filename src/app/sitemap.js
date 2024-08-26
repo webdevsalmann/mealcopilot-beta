50dc95e64d80f7006592c61bf900b62e44d37ca3
webdevsalmann <webdevsalman7869@gmail.com>
2024-08-26 15:40:05 +0530

    Update Sitemap

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -6,6 +6,25 @@ export default function sitemap() {
     return [
         {
             url: baseUrl,
+            priority: 1,
+        },
+        {
+            url: `${baseUrl}/careers`,
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.8,
+        },
+        {
+            url: `${baseUrl}/contact`,
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.6,
+        },
+        {
+            url: `${baseUrl}/how-it-works`,
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.4,
         },
     ]
 }
